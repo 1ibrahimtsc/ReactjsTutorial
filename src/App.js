ddf1028e051f9ce23634fc9d1c376f8db1aae044
IBRAHIM <1ibrahimtsc@gmail.com>
2019-05-20 22:06:31 +0600

    tutorial 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to React - Redux Complete Course</h1>
-      <p>lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. lorem25 text is content. </p>
-    </div>
-  );
+
+class App extends Component {
+  
+  render() {
+
+    let h = 'Welcome to React - Redux Complete Course .'
+    let p1 = 'Twinkle Cats .'
+    let p2 = 'This is a sample content .'
+    return (
+      <div className="App">
+        <h1> { h } </h1>
+        <p> hi {p1 ? p1 : 'no data'} {p2} </p>
+        
+      </div>
+    ); 
+   /*
+
+    return React.createElement('div', {className: 'App'}, [
+      React.createElement('h1', null, 'Welcome to React - Redux Complete Course'),
+      React.createElement('p', null, 'This is a sample content.'), 
+      React.createElement('p', {className: 'ptag'}, 'This is a sample content.'),
+    ] )
+ 
+    */
+
+  } 
+
+ 
 }
 
 export default App;
