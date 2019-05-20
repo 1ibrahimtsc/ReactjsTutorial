31d3af1cede9df2e5638b8501dab06f5ca01cd0a
IBRAHIM <1ibrahimtsc@gmail.com>
2019-05-20 23:35:45 +0600

    tutorial 5 practice

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import First from './First/First'
+
+class App extends Component {
+  
+  render() {
+
+    return (
+      <div className="App">
+        <First />
+        <First />
+        <First />
+        
+      </div>
+    ); 
+   
+  } 
+
+ 
+}
+
+export default App;
---
+++ src/components/First/First.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class First extends Component {
+
+    render(){
+      return (
+          <div>
+              <h1> I am Mr Ibrahim </h1>
+              <p> I am a Webdevloper </p>
+              <p> I am also a software programmer </p>
+          </div>
+      )
+      
+    }
+}
+
+export default First
