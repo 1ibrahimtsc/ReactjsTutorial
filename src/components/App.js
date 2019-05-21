0208621cebee555ee55f006df49bf97d183fbeed
ibrahim <1ibrahimtsc@gmail.com>
2019-05-21 20:50:41 +0600

    state tutorial 7.2

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,27 @@ import Counter from './Counter/Counter';
 
 class App extends Component {
   
+  state = {
+    person: [
+      { name: 'ibrahim', email: 'ibrahimvoc@yahoo.com', address: 'Mirpur, Dhaka' },
+      { name: 'Rony', email: 'Rony@yahoo.com', address: 'Danmondi, Dhaka' },
+      { name: 'Anis', email: 'Anis@yahoo.com', address: 'Mirpur, Dhaka' }
+    ]
+  }
+
   render() {
 
     return (
       <div className="App">
-        <First name='Mr Ibrahim' designation='software programmer and webdevloper' location='Dhaka, Bangladesh' />
+        { this.state.person.map((people, index) => {
+          return <First 
+                  key={ index } 
+                  name={ people.name } 
+                  email={ people.email } 
+                  address= { people.address } 
+                  />
+        }) }
 
-        <First name='Mr Rana' designation='webdegiener' location='Kustia, Bangladesh' />
-        
         <Counter />
       </div>
     ); 
