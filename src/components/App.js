65a0ef0b586dd80809192ecbf17e619eb8fe1c55
ibrahim <1ibrahimtsc@gmail.com>
2019-05-23 19:31:47 +0600

    List & event in react tutorial 11

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,61 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
+// import Books from './Books/Books';
 
 class App extends Component {
   
-  state = { name: '' }
+  state = { 
+    books: [ { name: 'Javascript', price: 20, id: 1  },
+      { name: 'React', price: 30, id: 2 },
+      { name: 'Java', price: 25, id: 3 },
+      { name: 'python', price: 35, id: 4 },
+      { name: 'Php', price: 45, id: 5 } ]
+   }
 
-  clickHandler = (event) => {
-   // alert('I have clicked')
-    console.log(event.target.value)
-  }
-
-  inputHandler = (event) => {
-    
-    this.setState({
-      name: event.target.value
-    }) 
-    //console.log(event.target.value)
-  }
-
-  render() {
+    render() {
 
     return (
       <div className="App">
-        <div className="container my-3">
-          <input onChange={ this.inputHandler }
-          type="text" value={ this.state.name } placeholder="Enter Your Name"
-           />
-          <button className="btn btn-primary" onClick={ (event) => console.log(event) } >Click Me</button>
-        </div>
-
-        { this.state.name ? <p>Hello Mr . { this.state.name } </p> : '' }
+        <Books books={ this.state.books } />
       </div>
     ); 
    
   } 
  
 }
+
+
+class Books extends Component {
+  render(){
+      return (
+          <div>
+              { this.props.books.map(book => {
+                  return (
+                      <Book name={ book.name } price={ book.price } />
+                  )
+              }) }
+          </div>
+      )
+  }
+}
+
+
+class Book extends Component {
+  render(){
+      return(
+          <li className='list-group-item d-flex' >
+             <p> { this.props.name } </p>
+             <span className='ml-auto'> ${ this.props.price } </span>
+             <div className='mx-4'>
+                 <span>
+                     <i className="fas fa-trash"></i>
+                 </span>
+             </div> 
+          </li>
+      )
+  }
+}
+
 
 export default App;
---
+++ src/components/Books/Book/Book.js
@@ -0,0 +1,21 @@
+/*
+import React, { Component } from "react";
+
+class Book extends Component {
+    render(){
+        return(
+            <li className='list-group-item d-flex' >
+               <p> { this.props.name } </p>
+               <span className='ml-auto'> ${ this.props.price } </span>
+               <div className='mx-4'>
+                   <span>
+                       <i className="fas fa-trash"></i>
+                   </span>
+               </div> 
+            </li>
+        )
+    }
+}
+
+export default Book;
+*/
